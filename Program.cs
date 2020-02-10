005354ceb661fdea10a0f2d777b78b6fed4f8f0b
Michael Keough <keoughm21@gmail.com>
2020-02-10 15:22:33 -0500

    'created project!'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace welcometo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello,World, my name is, Michael Keough");
+    }
+  }
+}
